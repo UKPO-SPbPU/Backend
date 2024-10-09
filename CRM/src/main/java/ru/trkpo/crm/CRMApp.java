c36d56c9a9f2bacf2b42e64b2fffe8ecf3e80239
Vsevolod Romashov <avs.rom@gmail.com>
2024-10-09 20:38:00 +0300

    Fix CRM service

--- CRM/src/main/java/ru/trkpo/crm/CRMApp.java
+++ CRM/src/main/java/ru/trkpo/crm/CRMApp.java
@@ -2,7 +2,10 @@ package ru.trkpo.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.client.RestTemplate;
+
 import ru.trkpo.common.CommonConfig;
 
 @SpringBootApplication
@@ -10,5 +13,10 @@ import ru.trkpo.common.CommonConfig;
 public class CRMApp {
     public static void main(String[] args) {
         SpringApplication.run(CRMApp.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTesmplate() {
+        return new RestTemplate();
     }
 }
